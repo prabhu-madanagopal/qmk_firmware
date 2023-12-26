daa844e85a031d023ed5631bbc1b5d9807cebdd4
prabhu-madanagopal <prabhu.madanagopal@gmail.com>
2023-12-26 15:34:41 -0800

    Revert "align"
    This reverts commit 4982ca21b86dba68dec4f43755a623e659b1edfd.

--- keyboards/handwired/dactyl_manuform/3x5_2_pico/3x5_2_pico.h
+++ keyboards/handwired/dactyl_manuform/3x5_2_pico/3x5_2_pico.h
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define XXX KC_NO
 
 #define LAYOUT_split_3x5_2_pico( \
-
     L00, L01, L02, L03, L04,     R04, R03, R02, R01, R00, \
     L10, L11, L12, L13, L14,     R14, R13, R12, R11, R10, \
     L20, L21, L22, L23, L24,     R24, R23, R22, R21, R20, \
